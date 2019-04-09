244375a83aa95be3e3426cd2a125ab5b6a2ce646
Michael Loney <mloney@protonmail.com>
2019-04-08 21:42:48 -0500

    Remove tests to allow teammates a chance to contribute

--- ShoppingCartTests.java
+++ ShoppingCartTests.java
@@ -18,24 +18,6 @@ public class ShoppingCartTests extends TestCase {
     }
 
     @Test
-    // When empty, the cart has 0 items
-    public void testZeroItemsWhenEmpty() {
-        Product thing = new Product("test", 5);
-        cart.addItem(thing);
-        cart.empty();
-        assertEquals(cart.getItemCount(), 0);
-    }
-
-    @Test
-    // When a new item is added, the number of items must be incremented
-    public void testIncrementItemsUponAdd() {
-        int itemCount = cart.getItemCount();
-        Product thing = new Product("test", 5);
-        cart.addItem(thing);
-        assertEquals(cart.getItemCount(), itemCount + 1);
-    }
-
-    @Test
     // When a new product is added, the new balance must be the sum of the previous balance plus the cost of the new product
     public void testAddingValueOfNewProduct() {
         Product thing = new Product("test", 5);
